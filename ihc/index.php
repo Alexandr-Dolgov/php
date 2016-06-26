8c33fd1331718b0668eddb3283066fce6923312b
Alexandr-Dolgov <aydolgov@gmail.com>
2016-06-26 23:01:21 +0500

    init

---
+++ ihc/index.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title>ihc homework</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+
+<form method="post" action="login.php">
+    <label>login<input type="text" name="login"></label>
+    <label>password<input type="text" name="password"></label>
+    <input type="submit" name="send" value="--->">
+</form>
+
+</body>
+
+</html>
---
+++ ihc/login.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+    <title>ihc homework</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+
+<?php
+$login = $_POST["login"];
+$password = $_POST["password"];
+//echo "введенное имя $login введенный пароль $password";
+
+//$handle = fopen("pass.txt", "r");
+//fclose($handle);
+
+$lines = file('pass.txt');
+
+?>
+
+</body>
+
+</html>
