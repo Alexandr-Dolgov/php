e8cba101453449d99560f8a5ee6335436f1c9601
alexd <alexd@z-inc.net>
2016-07-11 12:11:06 +0500

    login

--- ihc/login.php
+++ ihc/login.php
@@ -8,15 +8,25 @@
 <body>
 
 <?php
-$login = $_POST["login"];
-$password = $_POST["password"];
-//echo "введенное имя $login введенный пароль $password";
-
-//$handle = fopen("pass.txt", "r");
-//fclose($handle);
+$actualLogin = $_POST["login"];
+$actualPassword = $_POST["password"];
 
 $lines = file('pass.txt');
+$expectedLogin = $lines[0];
+$expectedPassword = $lines[1];
 
+echo "{$actualLogin} actualLogin <br>";
+echo "{$expectedLogin} expectedLogin <br>";
+echo "{$actualPassword} actualPassword <br>";
+echo "{$expectedPassword} expectedPassword <br>";
+
+if ((strcmp($actualLogin, $expectedLogin) == 0) and (strcmp($actualPassword, $expectedPassword) == 0)) {
+    echo "верно";
+    //нужно открыть страницу с инфой
+} else {
+    echo "неверно";
+    //нужно средиректить на index и сказать что "логин/пароль неверные, попробуйте ещё раз"
+}
 ?>
 
 </body>
