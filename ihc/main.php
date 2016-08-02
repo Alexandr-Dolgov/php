add0e50838702cf5d754380d134050ad1baa77a4
alexd <alexd@z-inc.net>
2016-08-02 12:34:41 +0500

    запуск jps из php
    не забыть прописать через visudo
    www-data        ALL=(ALL) NOPASSWD: /usr/bin/jps
    
    путь к jps достаётся через
    whereis jps
    там же, по полученному пути, можно убедиться что права у /usr/bin/jps а значит в коде нужно запускать от рута
    $res = exec("echo '$pass' | sudo -u root -S jps -v 2>&1", $output, $returnVar);

--- ihc/main.php
+++ ihc/main.php
@@ -7,7 +7,20 @@
 
 <body>
 
-    main page
+main page
+
+<?php
+$lines = file('syspass.txt');
+$pass = trim($lines[0]);
+
+$output = array();
+$returnVar = 0;
+$res = exec("echo '$pass' | sudo -u root -S jps -v 2>&1", $output, $returnVar);
+
+echo "";
+?>
+
+
 
 </body>
 
